99a0c497ce30b4788d8edab847a8c394622f32d8
Arunkumar0037 <99722798+Arunkumar0037@users.noreply.github.com>
2024-10-11 12:04:29 +0000

    Create new react app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Header from './componenets/header';
+
+function App() {
+  return (
+    <div className="App">
+<Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/componenets/header.js
@@ -0,0 +1,8 @@
+function Header(){
+
+    return (
+        <h1 style = {{color: "red"}}>Hi, I am header</h1>
+    )
+}
+
+export default Header
